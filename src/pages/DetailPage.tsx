318743a23897843b7068c535104164d160de2e1d
mia <125868397+miaaK@users.noreply.github.com>
2023-03-20 22:33:18 +0900

    feat: add tabs to show the information on pokemon
    about | stats | evolution

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,14 +1,25 @@
 // Detail view of selected Pokemon
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Tabs from '../components/Tabs';
 
 interface Params {
   id: string;
 }
 
+type Tab = 'about' | 'stats' | 'evolution';
+
 const DetailPage: React.FC = () => {
   const { id } = useParams<Params | any>();
-  return <div>Detail Page id: {id}</div>;
+  const [selectedTab, setSelectedTab] = useState<Tab>('about');
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+  return (
+    <div>
+      <Tabs tab={selectedTab} onClick={handleClick} />
+    </div>
+  );
 };
 
 export default DetailPage;
