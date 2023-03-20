96db1c370e484dea0fb2d6443e88b13faf8fbbb0
mia <125868397+miaaK@users.noreply.github.com>
2023-03-20 19:16:35 +0900

    feat: delete redundant line

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import Q from 'q';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -6,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 const queryClient = new QueryClient();
@@ -16,7 +15,7 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
