cddd1ab7f998806b124d9717e404ff71cae92d18
mia <125868397+miaaK@users.noreply.github.com>
2023-03-20 16:31:27 +0900

    feat: update css styling for main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,15 +3,40 @@ import React from 'react';
 import styled from '@emotion/styled';
 import PokemonList from '../components/PokemonList';
 
-const Base = styled.div``;
+const Base = styled.div`
+  padding: 12px 18px;
+  overflow: hidden;
+`;
 
-const Title = styled.div``;
+const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #d34f49;
+  font-weight: bold;
+`;
 
-const Description = styled.div``;
+const Description = styled.small`
+  color: #6b7280;
+  padding: 0;
+  margin: 16px 0 0 0;
+  display: block;
+`;
 
-const ImageWrapper = styled.div``;
+const ImageWrapper = styled.div`
+  position: fixed;
+  width: 288px;
+  height: 288px;
+  top: 0;
+  right: 0;
+  opacity: 0.4;
+  transform: translate(96px, -96px);
+`;
 
-const Image = styled.div``;
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
 const MainPage: React.FC = () => {
   return (
@@ -23,7 +48,7 @@ const MainPage: React.FC = () => {
       </Description>
       <PokemonList />
       <ImageWrapper>
-        <Image />
+        <Image src="/assets/pocketball.svg" />
       </ImageWrapper>
     </Base>
   );
